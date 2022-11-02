21971dc8dbe02f4ee8be1e0dfe25e337b1cd7e3f
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2022-11-02 15:54:04 +0500

    basic setup

---
+++ SpaceXRockets/Contollers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpaceXRockets
+//
+//  Created by Павел Кай on 02.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
